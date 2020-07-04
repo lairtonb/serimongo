63030e349725598de03afdc358331e8999150ce3
Lairton Ballin <8069733+lairtonb@users.noreply.github.com>
2020-07-03 22:44:09 -0300

    An attempt to create the log view screen

--- SeriMongo/src/app/app.module.ts
+++ SeriMongo/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { LogsComponent } from './logs/logs.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      HomeComponent
+      HomeComponent,
+      LogsComponent
    ],
    imports: [
       BrowserModule,
