579b06b21a140b17f9c1cff7f20b87da31ac1100
Lairton Ballin <8069733+lairtonb@users.noreply.github.com>
2020-11-15 10:26:43 -0300

    Restructure UI code and CSS

--- SeriMongo/src/app/app-routing.module.ts
+++ SeriMongo/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', redirectTo: ''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
