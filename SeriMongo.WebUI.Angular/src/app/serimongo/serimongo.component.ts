557a8ee05d1fd8d9dde12d62fc188737b4091156
Lairton Ballin <lballin@eshopworld.com>
2023-11-27 22:18:13 -0300

    Updated to Angular 16 and .NET 6

---
+++ SeriMongo.WebUI.Angular/src/app/serimongo/serimongo.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+import { LogEntry } from './log-entry';
+import { getLogEntries } from './mocks';
+
+@Component({
+  templateUrl: './serimongo.component.html',
+  styleUrls: ['./serimongo.component.css']
+})
+export class SeriMongoComponent implements OnInit {
+  title = 'SeriMongoUI';
+  logEntries: LogEntry[] = [];
+
+  selectedRow: LogEntry;
+
+  ngOnInit() {
+    this.logEntries = getLogEntries();
+  }
+
+  setClickedRow(log: LogEntry) {
+    this.selectedRow = log;
+    log.showDetails = !log.showDetails;
+  }
+
+  redException(key: string) {
+    if (key === 'Exception')
+      return 'Exception';
+    else
+      return '';
+  }
+
+}
